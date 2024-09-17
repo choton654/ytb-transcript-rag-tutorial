b12326d4261ee53667430df46594e11adf4d37b9
choton654 <choton654@gmail.com>
2024-09-17 21:26:51 +0530

    add model

--- src/app.js
+++ src/app.js
@@ -4,21 +4,24 @@ import { getYoutubeTranscript } from "./getYtbTranscript.js";
 import express from 'express';
 import { connectToAstraDb, initMongooseVideoModel } from './astradbMongoose.js';
 import dotenv from "dotenv"
+import { generateEmbedings } from './generateEmbedings.js';
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-connectToAstraDb()
-initMongooseVideoModel()
+// connectToAstraDb()
+// initMongooseVideoModel()
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
     // res.sendFile(__dirname + '/index.html');
-
-    const transcriptTxt = await getYoutubeTranscript('https://www.youtube.com/watch?v=qN_2fnOPY-M')
-    // console.log('---transcriptTxt---',transcriptTxt);
+    // https://www.youtube.com/watch?v=qN_2fnOPY-M
+    // 'https://www.youtube.com/watch?v=aO1-6X_f74M'
+    const transcriptTxt = await getYoutubeTranscript('https://www.youtube.com/watch?v=QdDoFfkVkcw')
+    const embds = await generateEmbedings(transcriptTxt)
+    console.log('---embedings---',embds);
     // var wstream = fs.createWriteStream('merge.txt');
     // wstream.write(transcriptTxt);
     res.send(transcriptTxt)
---
+++ src/generateEmbedings.js
@@ -0,0 +1,50 @@
+import { HfInference } from "@huggingface/inference";
+import { pipeline } from '@xenova/transformers';
+import {config} from 'dotenv';
+config({path:'.env'})
+const hf = new HfInference(process.env.HF_TOKEN);
+
+// const myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+
+
+export const generateEmbedings = async (prompt) => {
+
+    
+//// create embeddings  with hugging face with access token ////
+
+    // const output1 = await hf.featureExtraction({
+    //     model: 'sentence-transformers/all-MiniLM-L6-v2',
+    //     inputs: prompt,
+    //   });
+
+
+    
+//// create embeddings locally with transformer model ////
+
+    const extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
+    const output1 = await extractor(prompt, { pooling: 'mean', normalize: true });
+
+
+      return output1
+}
+
+
+
+// fetch(`https://generativelanguage.googleapis.com/v1beta/models/text-embedding-004:embedContent?key=${process.env.GOOGLE_API_KEY}`, {
+    //     method: "POST",
+    //     headers: myHeaders,
+    //     body: JSON.stringify({
+    //         "model": "models/text-embedding-004",
+    //         "content": {
+    //             "parts": [{
+    //                 "text": prompt
+    //             }],
+    //             "task_type": "RETRIEVAL_DOCUMENT",
+    //         },
+    //     }),
+    //     redirect: "follow"
+    // })
+    //     .then((response) => response.text())
+    //     .then((result) => console.log('---res---',result))
+    //     .catch((error) => console.error('---err---',error));
